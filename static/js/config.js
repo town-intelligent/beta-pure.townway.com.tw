0253f44d0d0143b714e093439ba9261f1d672f3e
yillkid <yillkid@gmail.com>
2022-04-21 18:34:29 +0800

    fix: revise API URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // Host
-HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://beta-eid-backend.townway.com.tw";
+HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.townway.com.tw";
