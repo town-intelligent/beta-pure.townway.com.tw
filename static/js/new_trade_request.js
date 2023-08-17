f35d77f55af421d85cad0f8270ffc33107a2ea8a
box72777 <127952485+box72777@users.noreply.github.com>
2023-08-17 20:59:15 +0800

    測試傳送資料到Django

--- static/js/new_trade_request.js
+++ static/js/new_trade_request.js
@@ -5,10 +5,10 @@ doneBtn.addEventListener("click", function () {
   var email = "yillkid@gmail.com";
   var type = "1";
   var name = document.getElementById("task_name").value; // "test001";
-  var overview = document.getElementById("overview").value;// "overview123";
+  var overview = document.getElementById("overview").value; // "overview123";
   var token = document.getElementById("token").value; // "test001";
-  var cover = getLocalStorage("task_cover");//`${TASK_COVER}`;
-  
+  var cover = getLocalStorage("task_cover"); // `${TASK_COVER}`;
+
   form.append("uuid", uuid);
   form.append("email", email);
   form.append("type", type);
@@ -16,7 +16,7 @@ doneBtn.addEventListener("click", function () {
   form.append("token", token);
   form.append("overview", overview);
   form.append("cover", cover);
-    //"url": `${HOST_URL_TPLANET_DAEMON}/tasks/new`,
+
   let settings = {
     "url": `http://127.0.0.1:8000/tasks/new`,
     "method": "POST",
@@ -24,11 +24,15 @@ doneBtn.addEventListener("click", function () {
     "processData": false,
     "mimeType": "multipart/form-data",
     "contentType": false,
-    "data": form
+    "data": form,
+    "headers": {
+      "X-Requested-With": "XMLHttpRequest",
+      "X-CSRFToken": getCookie('csrftoken'),  // 使用getCookie函數獲取CSRF標記值
+    }
   };
 
   $.ajax(settings).done(function (res) {
     console.log(res);
-    //window.location.replace("/verifier-cms-list.html");
+    // window.location.replace("/verifier-cms-list.html");
   });
 });
